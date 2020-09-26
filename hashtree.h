e9ebea5d68883bbf971151df352046ae6bd8046b
Leanne Dong <jdleanne@gmail.com>
2020-09-26 11:33:14 -0400

    getting started for hashtree

---
+++ hashtree.h
@@ -0,0 +1,18 @@
+/* Why hashing? A way of converting arbitrary data into a fixed size number
+ * For secure communication, it provides a way to check that the data hasn't been changed
+ * What do we need?
+ * struct node: node for storing an item in a binary tree
+ * struct bst: for storing a binary tree at each index of hash tree
+ * struct bst *arr
+ */
+
+#ifndef HASHTREE_HASHTREE_H
+#define HASHTREE_HASHTREE_H
+
+class hashtree {
+
+
+};
+
+
+#endif//HASHTREE_HASHTREE_H
